639635c52ab69c1eaae58d9734773f9ad8b4f917
TimBg <Best.Green.Zebra@yandex.ua>
2020-10-30 19:49:36 +0200

    added new functions

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-let array1 = [1, 2, 3, 0, 4, '', 5, 6, true, undefined, 'dfdf', false, null, ' '];
+let array1 = [1, 2, 3, 0, 4, '', 5, 6, true, undefined, 'dfdf', false, null, ' ', NaN];
 let array2 = [2, 3, false, true, 'dfgdfgdf', ''];
 
 const compact = arr => arr.filter(item => item);
@@ -11,10 +11,20 @@ const last = arr => arr[arr.length - 1];
 
 const join = (arr, sep) => arr.join(sep);
 
+const reverse = arr => arr.reverse();
+
+const concat = (...arrays) => arrays.reduce((acc, curr) => acc.concat(curr));
+
+const fill = (arr, val, start, end) => arr.fill(val, start, end);
+
+const nth = (arr, num) => num >= 0 ? arr[num] : arr[arr.length + num];
+
+const lastIndexOf = (arr, val) => arr.lastIndexOf(val);
+
 const difference = (arr1, arr2) => {
     let newArr = [];
 
-    for (let item of arr1) {
+    for (item of arr1) {
         if (!~arr2.findIndex(elem => elem === item)) newArr.push(item);
     }
 
@@ -38,8 +48,8 @@ const drop = (arr, quantity) => {
 
 const pull = (arr, ...values) => {
     return arr.filter(item => {
-        for (elem in values) {
-            if (values[elem] === item) return false;
+        for (elem of values) {
+            if (elem === item) return false;
         }
         return true;
     })
@@ -48,3 +58,63 @@ const pull = (arr, ...values) => {
 const remove = (arr, func) => {
     return difference(arr, arr.filter(func));
 }
+
+const take = (arr, quantity) => {
+    return arr.splice(0, quantity);
+}
+
+const union = (...arrays) => {
+    let resArray = [];
+
+    for (elem of [].concat(...arrays)) {
+        if (!resArray.includes(elem)) resArray.push(elem);
+    }
+
+    return resArray;
+}
+
+const flatten = arr => {
+    let newArr = [];
+
+    for (elem of arr) {
+        Array.isArray(elem) ? newArr.push(...elem) : newArr.push(elem);
+    }
+
+    return newArr;
+}
+
+const without = (arr, ...values) => {
+    return arr.filter(item => !values.includes(item));
+}
+
+const fromPairs = arr => {
+    let obj = {};
+
+    arr.forEach(item => { obj[item[0]] = item[1] });
+
+    return obj;
+}
+
+/*const flattenDeep = arr => {
+    let newArr = [];
+
+    for (elem of arr) {
+        Array.isArray(elem) ? newArr.push(...elem) : newArr.push(elem);
+    }
+
+    if (newArr.some(item => Array.isArray(item))) {
+        return flattenDeep(newArr)
+    } else {
+        return newArr;
+    };
+}*/
+
+/*const xor = (...arrays) => {
+    let resArray = [];
+
+    for (elem of [].concat(...arrays)) {
+        if (!resArray.includes(elem)) resArray.push(elem);
+    }
+
+    return resArray;
+}*/
